bac9bcd0410c5b3d60649393e1897e57236183a0
이준석 사원 <david.lee@curvc.com>
2021-03-01 18:30:00 +0900

    body-parser 사용 추가

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+
+// application/x-www-form-urlencoded 분석
+app.use(bodyParser.urlencoded({ extended : true }));
+
+// application/json 분석
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,3 +17,9 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+// path가 /register에 post 요청이 왔을 경우
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  // 우선은 무조건 success, 그리고 id값 반환
+  return res.json({success : true, id : req.body.id});
+})
