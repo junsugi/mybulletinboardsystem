f7eb2e4e39bb7f3801e667bb21fa2dc079391b73
이준석 <david.lee@curvc.com>
2021-03-02 21:45:56 +0900

    firebase 연동중

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+const { Router } = require('express');
+
+// html 사용할 경우
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
 
 // application/x-www-form-urlencoded 분석
 app.use(bodyParser.urlencoded({ extended : true }));
@@ -10,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+  console.log("call /")
   res.send('Hello World!')
 })
 
@@ -23,3 +33,30 @@ app.post("/register", (req, res) => {
   // 우선은 무조건 success, 그리고 id값 반환
   return res.json({success : true, id : req.body.id});
 })
+
+app.get("/firebase-test", (req, res) => {
+  res.render("firebase.ejs")
+})
+
+  // Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+const firebase = require("firebase/app");
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBwbgbYMHQ3AQjYfLYgMiAHYhTVuNGsT6c",
+  authDomain: "mybulletinboardsystem.firebaseapp.com",
+  databaseURL: "https://mybulletinboardsystem-default-rtdb.firebaseio.com",
+  projectId: "mybulletinboardsystem",
+  storageBucket: "mybulletinboardsystem.appspot.com",
+  messagingSenderId: "537073061673",
+  appId: "1:537073061673:web:17ce0e40925fa5dd163138"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+var test = {
+  name: "이준석",
+  email: "junsugi@velog",
+}
+
+var database = firebase.database();
